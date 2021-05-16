e931c72eb4825ba77eefac1c8efada7727dcb359
IWannaBeMovieMaker <Ondrejkud3la@seznam.cz>
2021-05-16 22:26:52 +0200

    0.4 - přidání overview a descriptions, úprava Homepage

--- manga/urls.py
+++ manga/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('topten', views.topten, name='topten'),
+    path('mangas/', views.MangaListView.as_view(), name='mangas'),
+    path('mangas/types/<str:type_name>/', views.MangaListView.as_view(), name='manga_type'),
+    path('mangas/<int:pk>/', views.MangaDetailView.as_view(), name='manga_detail'),
 ]
--- manga/views.py
+++ manga/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from manga.models import Manga, Attachment
+from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
+
+from manga.models import Manga, Type, Attachment
 
 
 def index(request):
@@ -7,7 +10,7 @@ def index(request):
 
     # Generate counts of some of the main objects
     num_mangas = Manga.objects.all().count()
-    mangas = Manga.objects.order_by('-rating')[:4]
+    mangas = Manga.objects.order_by('-rating')[:10]
 
     context = {
         'num_mangas': num_mangas,
@@ -17,12 +20,56 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
-def topten(request):
-    """
-    View function for home page of site.
-    """
-    # Render the HTML template index.html
-    return render(
-        request,
-        'topten.html',
-    )
+
+class MangaListView(ListView):
+    model = Manga
+
+    context_object_name = 'mangas_list'
+    template_name = 'manga/list.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        if 'genre_name' in self.kwargs:
+            return Manga.objects.filter(types__name=self.kwargs['type_name']).all()
+        else:
+            return Manga.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['num_mangas'] = len(self.get_queryset())
+        if 'type_name' in self.kwargs:
+            context['view_title'] = f"Type: {self.kwargs['type_name']}"
+            context['view_head'] = f"Manga Type: {self.kwargs['type_name']}"
+        else:
+            context['view_title'] = 'Mangas'
+            context['view_head'] = 'Manga overview'
+        return context
+
+
+class MangaDetailView(DetailView):
+    model = Manga
+
+    context_object_name = 'manga_detail'
+    template_name = 'manga/detail.html'
+
+
+class TypeListView(ListView):
+    model = Type
+    template_name = 'blocks/type_list.html'
+    context_object_name = 'types'
+    queryset = Type.objects.order_by('name').all()
+
+
+class TopTenListView(ListView):
+    model = Manga
+    template_name = 'blocks/top_ten.html'
+    context_object_name = 'mangas'
+    queryset = Manga.objects.order_by('-rating').all()[:10]
+
+
+class NewMangaListView(ListView):
+    model = Manga
+    template_name = 'blocks/new_mangas.html'
+    context_object_name = 'mangas'
+    queryset = Manga.objects.order_by('release_date').all()
+    paginate_by = 2
